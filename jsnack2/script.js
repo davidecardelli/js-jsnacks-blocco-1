4f3485ae5283ab2d9b2ebb9570fc05ed4089db7a
davidecardelli <davide.cardelli.94@gmail.com>
2022-12-14 22:29:02 +0100

    end ex 2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,1 +1,23 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, l’utente inserisce il suo nome tramite un input.comunicagli se può partecipare o no alla festa.
+
+const invitati = ["Aldo", "Giovanni", "Giacomo"];
+
+const userChoice = document.getElementById('userChoice');
+const listResult = document.getElementById('listResult');
+
+userChoice.addEventListener('click' , function(){
+
+    const userName = document.getElementById('userName').value;
+
+    isInvited = false;
+
+    for (let i = 0; i < invitati.length && !isInvited; i++) {
+        if (userName === invitati[i]) {
+            isInvited = true;
+        }
+    }
+
+    const message = isInvited ? 'Entra, sei invitato' : 'Sono spiacente, non sei invitato!';
+    console.log(message);
+    listResult.innerText = message;
+})
